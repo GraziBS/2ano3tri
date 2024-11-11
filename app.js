33e336a8994296c3a98a1c60276060c9efba8907
GraziBS <142225143+GraziBS@users.noreply.github.com>
2024-11-11 12:07:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-function criaCartao(categoria, pergunta, resposta){
+function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
 
     cartao.innerHTML = `
-    <div class="cartao_conteudo">
+    <div class="cartao__conteudo">
     <h3>${categoria}</h3>
-    <div class="cartao_conteudo_pergunta">
+    <div class="cartao__conteudo__pergunta">
         <p>${pergunta}</p>
     </div>
-    <div class="cartao_conteudo_resposta">
-      <p>${resposta}</p>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
     </div>
     </div>
     `
 
     let respostaEstaVisivel = false
 
-    function viraCartao(){
+    function viraCartao() {
         respostaEstaVisivel = !respostaEstaVisivel
         cartao.classList.toggle('active', respostaEstaVisivel)
     }
@@ -25,5 +25,5 @@ function criaCartao(categoria, pergunta, resposta){
 
 
     container.appendChild(cartao)
-    
+
 }
