0c6fabd70d0e1091c33a11045617735f84f99dfe
GraziBS <142225143+GraziBS@users.noreply.github.com>
2024-11-18 12:08:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ function criaCartao(categoria, pergunta, resposta){
     <div class="cartao_conteudo_resposta"
        <p>${resposta}</p>
     </div>
+    </div>
+    '
+
+    let respostaEstaVisivel = false
+
+    function viraCartao(){
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toogle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+
+
+    container.appendChild(cartao)
+
+}
