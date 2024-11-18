83e503034f18ba2d98a4d7f530f9db368b5546ac
GraziBS <142225143+GraziBS@users.noreply.github.com>
2024-11-18 10:38:21 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Programação'
+    'O que é Phyton?'
+    'O Phyton é uma linguagem de programação'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
+    'Geografia'
+    'Qual é a capital da França?'
     'A capital da França é Paris'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Programação'
+    'O que é uma função?'
+    'Uma função é um bloco de código que executa uma tarefa'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Lingua Inglesa'
+    'Como se diz OI em Inglês'
+    'OI em Inglês se diz HI (RAI)'
 )
